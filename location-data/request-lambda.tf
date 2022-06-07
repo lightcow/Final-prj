a6cae18931c9469049a176718bed73e405c0ebed
Olatte3 <76947477+Olatte3@users.noreply.github.com>
2022-06-07 10:30:27 +0900

    [REFACTOR] UPDATE request-lambda.tf

--- location-data/request-lambda.tf
+++ location-data/request-lambda.tf
@@ -96,5 +96,5 @@ resource "aws_lambda_permission" "apigwR" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.request_lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:ap-northeast-2:896673219691:${aws_api_gateway_rest_api.jkApi.id}/*/${aws_api_gateway_method.proxy.http_method}${aws_api_gateway_resource.proxy.path}"
+  source_arn = "arn:aws:execute-api:ap-northeast-2:${var.amazon_id}:${aws_api_gateway_rest_api.jkApi.id}/*/${aws_api_gateway_method.proxy.http_method}${aws_api_gateway_resource.proxy.path}"
 }
